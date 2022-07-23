88a879cdfd58ac21be80d52e0b210963794563b9
Rafael Freitas <vidarafael@hotmail.com>
2022-07-23 13:47:39 -0300

    configs for deploy

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,6 +2,6 @@ import { app } from "./app";
 import * as path from 'path';
 require("dotenv").config({ path: path.resolve(__dirname, '../../../../.env') })
 
-app.listen(process.env.APP_URL, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log("Server started 🚀🚀")
 })
